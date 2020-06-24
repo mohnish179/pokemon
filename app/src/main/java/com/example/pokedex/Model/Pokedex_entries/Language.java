f483265d6d19052d8da0e16bd126cb17ec767344
Mohnish <amohnishbalaji@gmail.com>
2020-06-24 12:22:50 +0530

    fixed bug- pokemon_flavour_texts

---
+++ app/src/main/java/com/example/pokedex/Model/Pokedex_entries/Language.java
@@ -0,0 +1,13 @@
+package com.example.pokedex.Model.Pokedex_entries;
+
+public class Language {
+    String name;
+
+    public Language(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
